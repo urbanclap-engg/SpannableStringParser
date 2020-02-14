1874dfcefcb16b62bfbcc2cd15f79e8823dd6f61
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-02-14 17:25:45 +0530

    migrated to android X

--- sample/src/main/java/com/hitanshudhawan/spannablestringparser/sample/MainActivity.kt
+++ sample/src/main/java/com/hitanshudhawan/spannablestringparser/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hitanshudhawan.spannablestringparser.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.text.method.LinkMovementMethod
 import com.hitanshudhawan.spannablestringparser.spannify
 import kotlinx.android.synthetic.main.activity_main.*
